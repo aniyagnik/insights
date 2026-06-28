13e97cb1a50c01b9582af64816bee07e771e71f6
aniyagnik <ani789yagnik@gmail.com>
2026-06-28 15:56:36 +0530

    update cors

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://insights-d176978v7-aniyagniks-projects.vercel.app"
+        "https://insights-flame.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
