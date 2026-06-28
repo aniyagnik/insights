63aaea0522a54d65882f33f2d1be4d00555d9798
aniyagnik <ani789yagnik@gmail.com>
2026-06-28 14:32:30 +0530

    deploy: add multi-process start script for free tier render compatibility

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+celery -A app.worker.celery_app worker --loglevel=info &
+
+celery -A app.worker.celery_app beat --loglevel=info &
+
+uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
